f8f530ef93069c58ac66f2e2421a434e9e1ca619
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-09-24 11:59:46 -0500

    CHART.JS EN BARRA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,75 @@
 console.log("Entro al main.js");
+const ENDPOINT_BASE = "http://ucamp.alumnos.dev4humans.com.mx/Main";
+const tblDatos = document.getElementById("tblDatos");
+const ctx = document.getElementById('myChart').getContext('2d');
+
+function loadData() {
+    console.log("entro a cargar data..");
+    fetch(ENDPOINT_BASE + "/endpoint_ingresos_mensuales",
+        {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(result => {
+            //console.log(result.data);
+            // console.log(result.data.map(item =>item.mes));
+            // console.log(result.data.map(item =>item.mes));
+
+            const labels_for_chart = result.data.map(item => item.nombre);
+            const data_for_chart = result.data.map(item => item.monto);
+            console.log(labels_for_chart);
+            console.log( data_for_chart);
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+               // type: 'line',
+                data: {
+                    labels: labels_for_chart,
+                    datasets: [{
+                        label: 'Datos Anuales',
+                        data: data_for_chart,
+                        backgroundColor: [
+                            '#E2CADF',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            '#543E51',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+            tblDatos.innerHTML = "";
+            for (const registro of result.data) {
+                let tr = `
+            <tr>
+                <td>${registro.id}</td>
+                <td>${registro.nombre.toUpperCase()}</td>
+                <td>${registro.monto}</td>
+            </tr>`;
+                tblDatos.innerHTML += tr;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+}
+
+loadData();
+
